0702c65e7d0db5177eb50905e9cbd8f9dac1d7d5
ronilsonbatista <ronilsondaconceicao@gmail.com>
2018-09-15 11:20:04 -0300

    Game View Controller

--- SpaceInvaders/SpaceInvaders/Features/GamePlay/GameViewController.swift
+++ SpaceInvaders/SpaceInvaders/Features/GamePlay/GameViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // Configure the view.
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        // Create and configure the scene.
+        let scene = GameScene(size: skView.frame.size)
+        skView.presentScene(scene)
+        
+        // Pause the view (and thus the game) when the app is interrupted or backgrounded
+        NotificationCenter.default.addObserver(self, selector: #selector(GameViewController.handleApplicationWillResignActive(_:)), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(GameViewController.handleApplicationDidBecomeActive(_:)), name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
     }
     
-
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    @objc func handleApplicationWillResignActive (_ note: Notification) {
+        let skView = self.view as! SKView
+        skView.isPaused = true
+    }
+    
+    @objc func handleApplicationDidBecomeActive (_ note: Notification) {
+        let skView = self.view as! SKView
+        skView.isPaused = false
+    }
+    
 }
