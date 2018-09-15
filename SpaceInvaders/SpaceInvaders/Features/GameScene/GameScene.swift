7d3c6700a49ebeebd668bf58c1a4f4a3f7245044
ronilsonbatista <ronilsondaconceicao@gmail.com>
2018-09-15 11:38:33 -0300

    Adding Colorful Invaders

--- SpaceInvaders/SpaceInvaders/Features/GameScene/GameScene.swift
+++ SpaceInvaders/SpaceInvaders/Features/GameScene/GameScene.swift
@@ -13,10 +13,30 @@ class GameScene: SKScene {
     
    var contentCreated = false
     
+    enum InvaderType {
+        case a
+        case b
+        case c
+        
+        static var size: CGSize {
+            return CGSize(width: 24, height: 16)
+        }
+        
+        static var name: String {
+            return "invader"
+        }
+    }
+    
+    let kInvaderGridSpacing = CGSize(width: 12, height: 12)
+    let kInvaderRowCount = 6
+    let kInvaderColCount = 6
+    
+    
     override func didMove(to view: SKView) {
         
         if (!self.contentCreated) {
-            self.createContent()
+//            self.createContent()
+            self.setupInvaders()
             self.contentCreated = true
         }
     }
@@ -27,6 +47,64 @@ class GameScene: SKScene {
         self.addChild(invader)
         self.backgroundColor = SKColor.black
     }
+    
+    func makeInvader(ofType invaderType: InvaderType) -> SKNode {
+        // 1
+        var invaderColor: SKColor
+        
+        switch(invaderType) {
+        case .a:
+            invaderColor = SKColor.red
+        case .b:
+            invaderColor = SKColor.green
+        case .c:
+            invaderColor = SKColor.blue
+        }
+        
+        // 2
+        let invader = SKSpriteNode(color: invaderColor, size: InvaderType.size)
+        invader.name = InvaderType.name
+        
+        return invader
+    }
+    
+    func setupInvaders() {
+        // 1
+        let baseOrigin = CGPoint(x: size.width / 3, y: size.height / 2)
+        
+        for row in 0..<kInvaderRowCount {
+            // 2
+            var invaderType: InvaderType
+            
+            if row % 3 == 0 {
+                invaderType = .a
+            } else if row % 3 == 1 {
+                invaderType = .b
+            } else {
+                invaderType = .c
+            }
+            
+            // 3
+            let invaderPositionY = CGFloat(row) * (InvaderType.size.height * 2) + baseOrigin.y
+            
+            var invaderPosition = CGPoint(x: baseOrigin.x, y: invaderPositionY)
+            
+            // 4
+            for _ in 1..<kInvaderColCount {
+                // 5
+                let invader = makeInvader(ofType: invaderType)
+                invader.position = invaderPosition
+                
+                addChild(invader)
+                
+                invaderPosition = CGPoint(
+                    x: invaderPosition.x + InvaderType.size.width + kInvaderGridSpacing.width,
+                    y: invaderPositionY
+                )
+            }
+        }
+    }
+
     
     // Scene Update
     override func update(_ currentTime: TimeInterval) {
